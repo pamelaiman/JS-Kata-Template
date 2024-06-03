ce92b4c8717b61bb757aa70da017f012e9be6631
Mohammed Adam <adamekissa@gmail.com>
2024-06-03 11:14:46 +0100

    Count down test added

---
+++ src/countDount.test.js
@@ -0,0 +1,17 @@
+import makeCountdownArray from "./countDount";
+
+
+test("COunting down from input number", () => {
+  
+    expect(makeCountdownArray(3, "🚀")).toEqual([3,2,1,"🚀"]);
+  });
+
+  test("COunting down from input number", () => {
+  
+    expect(makeCountdownArray(5, "💥")).toEqual([5, 4, 3,2,1,"💥"]);
+  });
+
+  test("COunting down from input number", () => {
+  
+    expect(makeCountdownArray(0, "💥")).toEqual(["💥"]);
+  });
